36c59644c4e9a72dff394a41d855fba486542b83
midtst0v <midtst0v@yahoo.com>
2026-02-10 13:37:29 -0600

    added simple playback control for BGM

--- webpage/js/music.js
+++ webpage/js/music.js
@@ -1,11 +1,36 @@
 let music = new Audio("assets/audio/qd-anoth.mp3");
+let playText = document.getElementById("nowplaying");
+let bgmButton = document.getElementById("bgmSwitch");
+
 music.loop = true;
-document.addEventListener("click", (event) => {
+bgmButton.addEventListener("click", (event) => {
   music.play();
 });
 
 music.addEventListener("playing", (event) => {
-  let playText = document.getElementById("nowplaying");
   let playStatus = "<strong><span>Now Playing:</strong> Quad - Another Worlds (qd-anoth.xm)</span>";
   playText.innerHTML = playStatus;
+
+  let bgmButton = document.getElementById("bgmSwitch");
+  bgmButton.innerHTML = "Pause BGM";
+  getPauseButton(bgmButton);
 });
+
+music.addEventListener("pause", (event) => {
+  bgmButton.innerHTML = "Play BGM";
+  getPlayButton(bgmButton);
+});
+
+function getPlayButton(bgmButton)
+{
+  bgmButton.addEventListener("click", (event) => {
+    music.play();
+  });
+}
+
+function getPauseButton(bgmButton)
+{
+  bgmButton.addEventListener("click", (event) => {
+    music.pause();
+  });
+}
