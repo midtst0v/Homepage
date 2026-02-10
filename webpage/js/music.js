6a04c8ca327749e2f2a210cce2a8e515d3fb1b69
midtst0v <midtst0v@yahoo.com>
2026-02-10 13:00:26 -0600

    fix: assigned event listener to wrong element + added playback text

--- webpage/js/music.js
+++ webpage/js/music.js
@@ -1,3 +1,9 @@
 let music = new Audio("assets/audio/qd-anoth.mp3");
-music.autoplay = true;
 music.loop = true;
+document.addEventListener("click", (event) => {
+  music.play();
+});
+
+music.addEventListener("playing", (event) => {
+  document.getElementById("nowplaying").innerHTML += "<strong><span>Now Playing:</strong> Quad - Another Worlds (qd-anoth.xm)</span>";
+});
