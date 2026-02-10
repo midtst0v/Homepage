f0089e27257666f8e05c1bdcecd47d9b787206af
midtst0v <midtst0v@yahoo.com>
2026-02-09 19:37:04 -0600

    changed header image

--- webpage/js/imagerow.js
+++ webpage/js/imagerow.js
@@ -7,7 +7,7 @@ let imgContainer = document.getElementById("images");
 let count = Math.ceil((head.length) / width);
 for (let i = 0; i < count; i++)
 {
-  imgContainer.innerHTML += "<img src='assets/img/run.gif'>";
+  imgContainer.innerHTML += "<img src='assets/img/ouch.gif'>";
 }
 
 // add UNDER CONSTRUCTION text to title
