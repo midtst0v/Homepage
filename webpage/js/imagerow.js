2ca152ea4e03f23fb33ca883e88c50de2ed67a20
midtst0v <midtst0v@yahoo.com>
2026-02-09 18:39:58 -0600

    UNDER CONSTRUCTION text is added dynamically

--- webpage/js/imagerow.js
+++ webpage/js/imagerow.js
@@ -1,10 +1,15 @@
 /* i could do this with a tiled background but i'm busy with other things right now */
-let width = 4; // appx width of image in chars
-let head = document.getElementsByTagName("h1")[0].innerHTML;
+const width = 4; // appx width of image in chars
+let head = document.getElementById("titleHead").innerHTML;
 let imgContainer = document.getElementById("images");
-let count = Math.ceil(head.length / width);
 
+// add a row of images above the title
+let count = Math.ceil((head.length) / width);
 for (let i = 0; i < count; i++)
 {
   imgContainer.innerHTML += "<img src='assets/img/run.gif'>";
 }
+
+// add UNDER CONSTRUCTION text to title
+let constrText = "[UNDER CONSTRUCTION]";
+document.getElementById("titleHead").innerHTML += "<br>" + constrText;
