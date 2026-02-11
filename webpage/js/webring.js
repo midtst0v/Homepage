0064caed9801a00c37352f90fc833c9d9ea4d7c2
midtst0v <midtst0v@yahoo.com>
2026-02-10 19:03:51 -0600

    webring: added some dumb error handling

--- webpage/js/webring.js
+++ webpage/js/webring.js
@@ -1,11 +1,19 @@
 async function populate()
 {
   const jsonUrl = "https://mdtx.cc/assets/json/webring.json";
-  const request = new Request(jsonUrl);
-  const response = await fetch(request);
-  const webRing = await response.json();
-  //const webRing = `{ "webring": [ {"alt" : "bodacious44", "url" : "https://bodacious44.net", "img" : "https://bodacious44.net/pics/8831/newbutton.gif"}, {"alt" : "mdtx.cc", "url" : "https://mdtx.cc", "img" : "https://mdtx.cc/assets/img/88x31.png"} ] }`;
-  populateWebring(webRing);
+  
+  try 
+  {
+    const request = new Request(jsonUrl);
+    const response = await fetch(request);
+    const webRing = await response.json();
+
+    populateWebring(webRing);
+  }
+  catch(error)
+  {
+    console.log("Web ring: Couldn't fetch JSON!");
+  }
 }
 
 function populateWebring(webring)
